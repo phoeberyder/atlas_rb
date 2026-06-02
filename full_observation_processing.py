da6573af4dfb06064a5c7ae13cd26cfcfc57d5ed
phoeberyder <phoebelucinda.ryder@gmail.com>
2026-06-02 15:00:26 +0100

    changing to next file

--- full_observation_processing.py
+++ full_observation_processing.py
@@ -7,7 +7,7 @@ from scipy.signal.windows import tukey, blackmanharris, boxcar, kaiser
 
 cpi = 128
 channel = 0
-infilename = '/share/nas2/pryder/SET_Observations_Test_1/Wednesday/vdifs/TSSat_20250205_lo1_1295MHz_intelsat33e.vdifb'
+infilename = '/share/nas2/pryder/SET_Observations_Test_1/Wednesday/vdifs/TSSat_20250205_lo1_1295MHz_intelsat33e.vdifc'
 window_function = 'tukey'
 zero_pad = 128
 
@@ -68,7 +68,7 @@ for n in range(number_of_strips):
     # Update TLE range rate for the start of this CPI
     s_offset = (start_idx / samp_rate)
     if target == 'intelsat':
-        t_tle = ts.utc(2025, 2, 5, 13, 55, s_offset+1)
+        t_tle = ts.utc(2025, 2, 5, 14, 0, s_offset+1)
     elif target == 'atlas':
         t_tle = ts.utc(2026, 2, 18, 14, 50, s_offset)
     range_rate = range_finder_general(tle, t_tle, telescope)[1]
@@ -140,4 +140,4 @@ for n in range(number_of_strips):
     
     spectrogram[:, n] = np.abs(doppler_spectrum)**2
 
-np.save('./spectrogram_b_intelsat_128cpi_1sampoverlap_tukey_128pad.npy', spectrogram)
+np.save('./spectrogram_c_intelsat_128cpi_1sampoverlap_tukey_128pad.npy', spectrogram)
