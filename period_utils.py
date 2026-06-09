df7196d24acd880745399378bee192eed8ecc75a
phoeberyder <phoebelucinda.ryder@gmail.com>
2026-06-09 11:17:24 +0100

    adding peak finder general

--- period_utils.py
+++ period_utils.py
@@ -1,16 +1,17 @@
 import numpy as np
 from scipy.signal import find_peaks
 
-def peakfindergeneral(x, y):
+def peakfindergeneral(x, y, threshold):
     '''
     Inputs:
         x: 1d array of lomb scargle frequencies
         y: 1d array of power of each frequency
+        threshold: factor of max power to check for peaks above
         
     Outputs:
         peak_freqs
         peak_periods'''
-    peaks = find_peaks(y, height=np.max(y)*0.1)
+    peaks = find_peaks(y, height=np.max(y)*threshold)
     peak_freqs = x[peaks[0]]
     print("Peak frequencies (Hz):", peak_freqs)
     peak_periods = 1/peak_freqs
