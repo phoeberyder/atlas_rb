f0a0d2af215a34d1735448e2b7a2ecdc029739cc
phoeberyder <phoebelucinda.ryder@gmail.com>
2026-06-09 11:19:52 +0100

    moving gaussian fitter to period utils

--- period_utils.py
+++ period_utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.signal import find_peaks
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
 
 def peakfindergeneral(x, y, threshold):
     '''
@@ -17,3 +19,32 @@ def peakfindergeneral(x, y, threshold):
     peak_periods = 1/peak_freqs
     print("Corresponding periods (s):", peak_periods)
     return peak_freqs, peak_periods
+
+def gaussian(x, A, mu, sigma):
+    beamwidth = 0.106
+    return A * np.exp(-(x - mu)**2 / (2 * sigma**2))
+
+def single_gaussian_fitter(f, p, init_mu, init_sigma, target_name):
+    
+    # power_range = np.sum(data[:, start_range:end_range], axis=1)
+    popt, pcov = curve_fit(gaussian, f, p, p0=[max(p), init_mu, init_sigma])
+    A, mu, sigma = popt
+
+    errors = np.sqrt(np.diag(pcov))
+    mu_err = errors[1]
+    # sigma_err = errors[2]
+
+    x_fit = f
+    y_fit = gaussian(f, *popt)
+    print('Peak offset = ', mu, 'Hz')
+    print('Error = ±', mu_err, 'Hz')
+    print('Width = ', sigma, 'Hz')
+    print('percentage error', sigma/f[np.argmax(p)]*100, '%')
+    plt.plot(f, p, label = 'observation')
+    plt.plot(x_fit, y_fit, label="Gaussian fit", color ='r')
+    plt.xlim(0.05, 0.075)
+    plt.xlabel('Frequency')
+    plt.ylabel('Power')
+    plt.title(target_name)
+    plt.legend()
+    plt.show()
